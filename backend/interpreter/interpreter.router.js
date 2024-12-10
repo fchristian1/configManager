b677caa3c8ba75e8dc618c93ec6bf8c18217f4d2
fchristian1 <fchristian1@gmx.net>
2024-12-10 02:06:21 +0100

    interpreter with links

--- backend/interpreter/interpreter.router.js
+++ backend/interpreter/interpreter.router.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
 
 const router = (interpreterService) => {
+    const systems = interpreterService.getSystems();
     const router = Router();
     router.get("/", (req, res) => {
-        const systems = interpreterService.getSystems();
         res.json({ systems });
+    });
+    router.get("/:systemName", (req, res) => {
+        const systemName = req.params.systemName;
+        const rootElements = interpreterService.getRootElements(systemName);
+        res.json(rootElements);
+    });
+    router.get("/:systemName/:id", (req, res) => {
+        const systemName = req.params.systemName;
+        const id = req.params.id;
+        const elements = interpreterService.getElementsFromId(systemName, id);
+        res.json(elements);
     });
     return router;
 };
